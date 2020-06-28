0bb3e99f74763b3b54bb49ec39d83e074c6ed576
Derek Zhan <derekjan1240@gmail.com>
2020-06-28 23:28:21 +0800

    handling room

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const io = socketIO.listen(server);
 
 io.on('connection', (socket) => {
     console.log('made socket connection', socket.id);
- 
+
     socket.on('sdp', (data) => {
         "use strict";
         // console.log(data);
@@ -31,4 +31,42 @@ io.on('connection', (socket) => {
         console.log('handle candidate!')
         socket.broadcast.emit('msg', data);
     });
+
+    // gameRoom
+    socket.on('findRoom', (data)=>{
+        io.of('/').in(data.roomName).clients((error: any, clients: string | any[]) => {
+            if (error) throw error;
+            if (clients.length === 1) {
+                socket.emit('isRoomExist', { result: true, msg: '加入房間!', room: data.roomName, user: data.userName });
+            } else if (clients.length > 1) {
+                socket.emit('isRoomExist', { result: false, msg: '房間已滿!' });
+            } else {
+                // socket.emit('isRoomExist', { result: false, msg: '找不到你說的房間哦!' });
+                socket.emit('autoCreateRoom', { result: true, msg: '自動生成房間!', room: data.roomName, user: data.userName });
+            }
+        })
+    });
+
+    socket.on('joinRoom', (data) => {
+        io.of('/').in(data.roomName).clients((error: any, clients: string | any[]) => {
+            if (error) throw error;
+            if (clients.length < 2) {
+                console.log(clients.length, data.roomName);
+                socket.join(data.roomName);
+
+                // test room msg
+                socket.in(data.roomName).broadcast.emit('test', { msg: 'room msg test', clientData: data });
+
+
+            } else {
+                // The room is full
+                socket.emit('isRoomExist', { result: false, msg: '房間已滿!' });
+            }
+        });
+    });
+});
+
+const room = io.of('/WebRTC/:roomID');
+room.on('connection', (socket) => {
+  console.log('someone connected to room', socket);
 });
